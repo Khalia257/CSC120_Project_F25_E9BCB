3e7cc10a8f0f74d2b401ebd340bb26d9501d2a5b
Khalia <kwashington11@broncos.uncfsu.edu>
2025-09-26 11:06:31 -0400

    name game and character name

---
+++ main.py
@@ -0,0 +1,9 @@
+game_name = "Escaping"  # TODO, name your game project
+print(f"Welcome to {game_name}")    # TODO, print greetings with game name
+print("======================") # TODO, add or remove equal symbol to align the greetings
+
+# Ask for the character's name
+name = input("Before we begin, what's your charcter's name?\n> ")
+
+# Print the name
+print(f"Great, {name}! Let's begin the adventure!")
